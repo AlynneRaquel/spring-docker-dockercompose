a5cc87aecfeca1889e87151525852281465c899b
AlynneRaquel <alynneraquel@hotmail.com>
2023-10-29 20:26:10 -0300

    First commit

---
+++ src/main/java/com/example/springdockerdockercompose/SpringDockerDockercomposeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdockerdockercompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerDockercomposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerDockercomposeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdockerdockercompose/SpringDockerDockercomposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdockerdockercompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerDockercomposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
